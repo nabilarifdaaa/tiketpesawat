04f4c84768791f26928c8825b499904eac9ed265
danisyahrul <danisyahrul31@gmail.com>
2018-05-07 21:19:25 +0300

    read bisa tapi yang update gk bisa

--- application/views/Penumpang/formView.php
+++ application/views/Penumpang/formView.php
@@ -6,11 +6,6 @@
 	<input type="text" name="form-control" value="<?php echo $penumpang['KodePesawat'] ?>" readonly="">
 	<input type="text" name="form-control" value="<?php echo $penumpang['Nama'] ?>" readonly="">
 	<input type="text" name="form-control" value="<?php echo $penumpang['KTP'] ?>" readonly="">
-	<input type="text" name="form-control" value="<?php echo $penumpang['Email'] ?>" readonly="">
-	<input type="text" name="form-control" value="<?php echo $penumpang['Nohp'] ?>" readonly="">
-	<input type="text" name="form-control" value="<?php echo $penumpang['JumlahTiket'] ?>" readonly="">
-	<input type="text" name="form-control" value="<?php echo $penumpang['TempatDuduk'] ?>" readonly="">
-	<input type="text" name="form-control" value="<?php echo $penumpang['Totalharga'] ?>" readonly="">
 	<a href="<?php echo base_url().'penumpang/edit/'.$penumpang['KodeBook'] ?>">Edit</a>
 	<a href="<?php echo base_url().'penumpang/delete/'.$penumpang['KodeBook'] ?>">Delete</a>
 </div>
