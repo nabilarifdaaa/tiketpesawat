75fbf7087d19ddc2f03436f6e4e46f68f59e0963
danisyahrul <danisyahrul31@gmail.com>
2018-05-14 11:48:16 +0300

    [ CRUD ] Tabel Pesawat

--- application/controllers/Pesawat.php
+++ application/controllers/Pesawat.php
@@ -20,6 +20,15 @@ class Pesawat extends CI_Controller {
 		$this->load->view('Templates/Footer');
 	} 
 
+	public function read(){
+		$data['result'] = $this->PesawatModel->getall();
+		
+
+		$this->load->view('Templates/Header');
+		$this->load->view('Pesawat/read',$data);
+		$this->load->view('Templates/Footer');
+	}
+
 	public function caripesawat(){
 		//tampil data pesawat
 		// $data['result2'] = $this->PesawatModel->getPesawat2();
@@ -51,5 +60,205 @@ class Pesawat extends CI_Controller {
 		$this->load->view('Templates/Header');
 		$this->load->view('Pesawat/DataTabel',$data);
 		$this->load->view('Templates/Footer');
+	}
+
+	public function deletePesawat($id) {
+		$this->PesawatModel->deletePesawat($id);
+
+		redirect('pesawat/read');
+	}
+
+	public function editPesawat($id) {
+		$data['result'] = $this->PesawatModel->getPesawat4($id);
+		
+		$this->load->view('Templates/Header');
+		$this->load->view('Pesawat/update',$data);
+		$this->load->view('Templates/Footer');
+	}
+
+	public function addPesawat() {
+		$this->load->view('Templates/Header');
+		$this->load->view('Pesawat/add');
+		$this->load->view('Templates/Footer');
+	}
+
+	public function doAdd() {
+		$inputKodePesawat = $_POST['inputKodePesawat'];
+		$inputMaskapai = $_POST['inputMaskapai'];
+		$inputBoarding = $_POST['inputBoarding'];
+		$inputLanding = $_POST['inputLanding'];
+		$inputKotaAsal = $_POST['inputKotaAsal'];
+		$inputKotaTujuan = $_POST['inputKotaTujuan'];
+		$inputKelas = $_POST['inputKelas'];
+		$inputHarga = $_POST['inputHarga'];
+
+		if($_FILES['inputFoto']['name'] != '') {
+
+			$target_dir = "uploads/";
+			$target_file = $target_dir . basename($_FILES["inputFoto"]["name"]);
+			$uploadOk = 1;
+			$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+			// Check if image file is a actual image or fake image
+			if(isset($_POST["submit"])) {
+			    $check = getimagesize($_FILES["inputFoto"]["tmp_name"]);
+			    if($check !== false) {
+			        echo "File is an image - " . $check["mime"] . ".";
+			        $uploadOk = 1;
+			    } else {
+			        echo "File is not an image.";
+			        $uploadOk = 0;
+			    }
+			}
+			// Check if file already exists
+			if (file_exists($target_file)) {
+			    echo "Sorry, file already exists.";
+			    $uploadOk = 0;
+			}
+			// Check file size
+			if ($_FILES["inputFoto"]["size"] > 500000) {
+			    echo "Sorry, your file is too large.";
+			    $uploadOk = 0;
+			}
+			// Allow certain file formats
+			if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+			&& $imageFileType != "gif" ) {
+			    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+			    $uploadOk = 0;
+			}
+			// Check if $uploadOk is set to 0 by an error
+			if ($uploadOk == 0) {
+			    echo "Sorry, your file was not uploaded.";
+			// if everything is ok, try to upload file
+			} else {
+			    if (move_uploaded_file($_FILES["inputFoto"]["tmp_name"], $target_file)) {
+			    	$foto = $_FILES["inputFoto"]["name"];
+
+			    	$data = array(
+			    		'KodePesawat' => $inputKodePesawat,
+			    		'Maskapai' => $inputMaskapai,
+			    		'Boarding' => $inputBoarding,
+			    		'Landing' => $inputLanding,
+			    		'FK_IdAsal' => $inputKotaAsal,
+			    		'FK_IdTujuan' => $inputKotaTujuan,
+			    		'Kelas' => $inputKelas,
+			    		'Harga' => $inputHarga,
+			    		'LogoPesawat' => $foto
+			    	);
+
+			    	$this->PesawatModel->addPesawat($data);
+
+			        echo "The file ". basename( $_FILES["inputFoto"]["name"]). " has been uploaded.";
+
+			        redirect('pesawat/read');
+			    } else {
+			        echo "Sorry, there was an error uploading your file.";
+			    }
+			}
+		} else {
+			$data = array(
+	    		'KodePesawat' => $inputKodePesawat,
+	    		'Maskapai' => $inputMaskapai,
+	    		'Boarding' => $inputBoarding,
+	    		'Landing' => $inputLanding,
+	    		'FK_IdAsal' => $inputKotaAsal,
+	    		'FK_IdTujuan' => $inputKotaTujuan,
+	    		'Kelas' => $inputKelas,
+	    		'Harga' => $inputHarga
+	    	);
+
+	    	$this->PesawatModel->addPesawat($data);
+
+	        redirect('pesawat/read');
+		}
+	}
+
+	public function doEdit() {
+		$inputKodePesawat = $_POST['inputKodePesawat'];
+		$inputMaskapai = $_POST['inputMaskapai'];
+		$inputBoarding = $_POST['inputBoarding'];
+		$inputLanding = $_POST['inputLanding'];
+		$inputKotaAsal = $_POST['inputKotaAsal'];
+		$inputKotaTujuan = $_POST['inputKotaTujuan'];
+		$inputKelas = $_POST['inputKelas'];
+		$inputHarga = $_POST['inputHarga'];
+
+		if($_FILES['inputFoto']['name'] != '') {
+
+			$target_dir = "uploads/";
+			$target_file = $target_dir . basename($_FILES["inputFoto"]["name"]);
+			$uploadOk = 1;
+			$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+			// Check if image file is a actual image or fake image
+			if(isset($_POST["submit"])) {
+			    $check = getimagesize($_FILES["inputFoto"]["tmp_name"]);
+			    if($check !== false) {
+			        echo "File is an image - " . $check["mime"] . ".";
+			        $uploadOk = 1;
+			    } else {
+			        echo "File is not an image.";
+			        $uploadOk = 0;
+			    }
+			}
+			// Check if file already exists
+			if (file_exists($target_file)) {
+			    echo "Sorry, file already exists.";
+			    $uploadOk = 0;
+			}
+			// Check file size
+			if ($_FILES["inputFoto"]["size"] > 500000) {
+			    echo "Sorry, your file is too large.";
+			    $uploadOk = 0;
+			}
+			// Allow certain file formats
+			if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+			&& $imageFileType != "gif" ) {
+			    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+			    $uploadOk = 0;
+			}
+			// Check if $uploadOk is set to 0 by an error
+			if ($uploadOk == 0) {
+			    echo "Sorry, your file was not uploaded.";
+			// if everything is ok, try to upload file
+			} else {
+			    if (move_uploaded_file($_FILES["inputFoto"]["tmp_name"], $target_file)) {
+			    	$foto = $_FILES["inputFoto"]["name"];
+
+			    	$data = array(
+			    		'KodePesawat' => $inputKodePesawat,
+			    		'Maskapai' => $inputMaskapai,
+			    		'Boarding' => $inputBoarding,
+			    		'Landing' => $inputLanding,
+			    		'FK_IdAsal' => $inputKotaAsal,
+			    		'FK_IdTujuan' => $inputKotaTujuan,
+			    		'Kelas' => $inputKelas,
+			    		'Harga' => $inputHarga,
+			    		'LogoPesawat' => $foto
+			    	);
+
+			    	$this->PesawatModel->updatePesawat($data, $inputKodePesawat);
+
+			        echo "The file ". basename( $_FILES["inputFoto"]["name"]). " has been uploaded.";
+
+			        redirect('pesawat/read');
+			    } else {
+			        echo "Sorry, there was an error uploading your file.";
+			    }
+			}
+		} else {
+			$data = array(
+	    		'KodePesawat' => $inputKodePesawat,
+	    		'Maskapai' => $inputMaskapai,
+	    		'Boarding' => $inputBoarding,
+	    		'Landing' => $inputLanding,
+	    		'FK_IdAsal' => $inputKotaAsal,
+	    		'FK_IdTujuan' => $inputKotaTujuan,
+	    		'Kelas' => $inputKelas,
+	    		'Harga' => $inputHarga
+	    	);
+
+	    	$this->PesawatModel->updatePesawat($data, $inputKodePesawat);
+
+	        redirect('pesawat/read');
+		}
 	}
 }
--- application/views/Pesawat/read.php
+++ application/views/Pesawat/read.php
@@ -1,8 +1,7 @@
-
+<!-- 
 <!DOCTYPE html>
 <html lang="en">
     <head>
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
@@ -16,12 +15,13 @@
     </style>
     
     </head>
-    <body>
+    <body> -->
         <div class="container">
-            <h4>Pencarian</h4>
         <h1>Data Pesawat</h1>
         <?php echo "<h2>".$this->session->flashdata('pesan')."</h2>" ?>
-        
+        <br/>
+
+        <a href="<?php echo base_url()."pesawat/addPesawat"; ?>"><button class="btn btn-success"><i class="glyphicon glyphicon-plus"></i>Add Pesawat</button></a>
         <table id="example" class="table table-hover">
             <thead>
 				<tr>
@@ -30,35 +30,33 @@
 	                <th>Landing</th>
 	                <th>Boarding</th>
 	                <th>Kota Asal</th>
-	               <th>Bandara Asal</th>
-	                <th>Kota Tujuan</th>
-	                <th>Bandara Tujuan</th>
+	                <th>Kota Tujuan</th> 	
 	                <th>Kelas</th>
 	                <th>Harga</th>
+	                <th>Logo Pesawat</th>
 	                <th>Action</th>
 				</tr>
 				</thead>
 				<tbody>
-					<?php foreach ($result3 as $data) { ?>
+					<?php foreach ($result as $data) { ?>
 		            <tr>
 		                <td><?php echo $data['KodePesawat']; ?></td>
 		                <td><?php echo $data['Maskapai']; ?></td>
 		                <td><?php echo $data['Landing']; ?></td>
 		                <td><?php echo $data['Boarding']; ?></td>
-		                	<?php foreach ($result1 as $data1) { ?>
-				                <td><?php echo $data1['NamaKota']; ?></td>
-				                <td><?php echo $data1['Bandara']; ?></td>
-				            <?php } ?>
-		                	<?php foreach ($result2 as $data2) { ?>
-		                		<td><?php echo $data2['NamaKota']; ?></td>
-		                		<td><?php echo $data2['Bandara']; ?></td>
-		                	<?php } ?> 
+		                <td><?php echo $data['FK_IdAsal']; ?></td>
+		                <td><?php echo $data['FK_IdTujuan']; ?></td>
 		                <td><?php echo $data['Kelas']; ?></td>
 		                <td><?php echo $data['Harga']; ?></td>
+		                <td><img src="<?php echo base_url() . "uploads/" . $data['LogoPesawat']; ?>" alt="" width="100" /></td>
 		                <td>	
-		                	<a href="<?php echo base_url()."Penumpang/pilih/".$data['KodePesawat']; ?>">
+		                	<a href="<?php echo base_url()."Pesawat/editPesawat/".$data['KodePesawat']; ?>">
 		                	<button class="btn btn-primary">
-		                	Pilih</button>
+		                	Edit</button>
+		                	</a>
+		                	<a href="<?php echo base_url()."Pesawat/deletePesawat/".$data['KodePesawat']; ?>">
+		                	<button class="btn btn-primary">
+		                	Delete</button>
 		                	</a>
 		                </td>
 		            </tr>
@@ -66,14 +64,13 @@
 				</tbody>
         </table>
     </div>
-    </body>
+    <!-- </body>
 <script type="text/javascript">
 $(document).ready(function() {
     $('#example').DataTable();
 } );
 </script>
-
 <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap.min.js"></script>
-</html>
+</html> -->
